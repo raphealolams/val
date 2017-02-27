82f171b3c49dd41f0e324d5678119624faf80031
Ajilore Raphael Olamide <raphealolams@gmail.com>
2017-02-27 21:22:04 +0100

    new

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* 
+
+Oh nice, welcome to the js file of dreams.
+Enjoy responsibly!
+@ihatetomatoes
+
+*/
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+});
